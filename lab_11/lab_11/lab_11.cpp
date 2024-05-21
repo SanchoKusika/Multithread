4ecd98c3a1aabbf1dd549c4dbdaada250d549e2e
Aleksandr <99037228+SanchoKusika@users.noreply.github.com>
2024-05-21 15:21:16 +0300

    Add files via upload

---
+++ lab_11/lab_11/lab_11.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <Windows.h>
+#include <semaphore>
+
+std::mutex mtx;  // Один мьютекс для защиты вывода
+std::condition_variable cv;  // Переменная условия для сигнализации
+bool ready_to_switch = true;  // Условная переменная для смены потоков
+int current_thread = 1;  // Текущий активный поток
+
+void printThread(int id, int max) {
+    for (int i = 1; i <= max; i++) {
+        std::unique_lock<std::mutex> lk(mtx);
+        cv.wait(lk, [&]() { return current_thread == id; });  // Ожидание своей очереди
+        std::cout << "Поток " << id << ": Строка " << i << std::endl;
+        current_thread = id == 1 ? 2 : 1;  // Переключение на другой поток
+        lk.unlock();
+        cv.notify_all();  // Оповещение другого потока
+    }
+}
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    const int max_lines = 10;
+
+    std::thread t1(printThread, 1, max_lines);
+    std::thread t2(printThread, 2, max_lines);
+
+    t1.join();
+    t2.join();
+
+    return 0;
+}
+
+// 12:
+// Deadlock
+// Race condition
+// Неэффективное использование ресурсов
+// 
+// 
+// решение задания подходит к 13 лаб
---
+++ lab_14/lab_14/lab_14.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <thread>
+#include <semaphore>
+#include <windows.h>
+
+std::counting_semaphore<1> sem1(1);
+std::counting_semaphore<1> sem2(0);
+
+void printThread(int id, int max) {
+    for (int i = 1; i <= max; i++) {
+        if (id == 1) {
+            sem1.acquire();  // Ждем сигнала от семафора 1
+            std::cout << "Поток 1: Строка " << i << std::endl;
+            sem2.release();  // Отправляем сигнал семафору 2
+        }
+        else {
+            sem2.acquire();  // Ждем сигнала от семафора 2
+            std::cout << "Поток 2: Строка " << i << std::endl;
+            sem1.release();  // Отправляем сигнал семафору 1
+        }
+    }
+}
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    const int max_lines = 10;
+
+    std::thread t1(printThread, 1, max_lines);
+    std::thread t2(printThread, 2, max_lines);
+
+    t1.join();
+    t2.join();
+
+    return 0;
+}
+
+
+//15
+// семафоры контролируют порядок доступа потоков к ресурсам
